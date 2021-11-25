4a72e3ba57c4f527669bd7ca4f2726fc0dfbf0d7
zy <597172583@qq.com>
2021-11-25 09:00:13 +0800

    WS-2021.2.3 <59717@zhangyang Create baseRefactoring.xml, markdown.xml, laf.xml, Vue Single File Component.vue, ide.general.xml, Windows.xml, scDialog 模板.vue, Default.xml, find.xml, Windows 2.xml, one_dark_config.xml, ui.lnf.xml, wsl.distributions.xml, filetypes.xml, keymap.xml, lermitage-extra-icons.xml, vcs.xml, ZY.xml, colors.scheme.xml, notifications.xml, editor.xml, templates.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="${NAME}"></div>
+</template>
+
+<script>
+export default {
+name:"${NAME}",
+data() {
+return {}
+},
+}
+</script> 
+<style lang='scss' scoped>
+.${NAME}{}
+</style>
---
+++ fileTemplates/scDialog 模板.vue
@@ -0,0 +1,48 @@
+<template>
+    <sc-dialog :title="`XXXXX`" v-model="propValue" custom-class="${NAME}" width="70vw" top="50px" @close="\$emit('input', false)" @opem='open'>
+        <div class="dialog-content pl30 pr30">dialog-content</div>
+        <span slot="footer" class="dialog-footer mt30">
+            <el-button size="mini" @click="_close">取 消</el-button>
+            <el-button size="mini" type="primary" @click="_close">确 定</el-button>
+        </span>
+    </sc-dialog>
+</template>
+<script>
+export default {
+    name:"${NAME}",
+    props: {
+        value: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {};
+    },
+    computed: {
+        propValue: {
+            get() {
+                return this.value;
+            },
+            set(nval) {
+                this.\$emit("input", nval);
+            }
+        }
+    },
+    methods: {
+        //关闭弹窗
+        _close() {
+            this.\$emit("input", false);
+        },
+        //打开痰弹窗触发 //获取接口
+        open(){
+        },
+    }
+};
+</script>
+<style lang="scss" scoped>
+.${NAME} {
+    .dialog-content {
+    }
+}
+</style>
